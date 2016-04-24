b4bcf6019515f2d2db8b78e69897b39d1186aed5
Berend Wouters <berend.wouters@gmail.com>
2016-04-24 16:43:11 +0200

    Try to update database using git deploy

--- ImageGallery/Startup.cs
+++ ImageGallery/Startup.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.Owin;
+﻿using System.Data.Entity.Migrations;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(ImageGallery.Startup))]
+
 namespace ImageGallery
 {
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
+            UpdateDatabase();
             ConfigureAuth(app);
+        }
+
+        private static void UpdateDatabase()
+        {
+            var configuration = new ImageGallery.Migrations.Configuration();
+            var migrator = new DbMigrator(configuration);
+            migrator.Update();
         }
     }
 }
