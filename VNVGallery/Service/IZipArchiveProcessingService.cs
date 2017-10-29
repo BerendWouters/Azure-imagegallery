5f500e841c12c4f1ec1e9aafd0e35ac32d9c9c20
Berend Wouters <berend.wouters@gmail.com>
2017-10-29 17:49:10 +0100

    Adds unity.mvc to the solution

---
+++ VNVGallery/Service/IZipArchiveProcessingService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+using System.Collections.Generic;
+using VnVGallery.Data;
+
+namespace VnVGallery.Service
+{
+    public interface IZipArchiveProcessingService
+    {
+        IReadOnlyList<FileDetail> ProcessFiles(CloudBlobContainer container, string path);
+    }
+}
