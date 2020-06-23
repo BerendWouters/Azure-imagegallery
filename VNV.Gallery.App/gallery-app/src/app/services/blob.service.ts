c9038d2ebfd7dd2d02dea9f044564061d4ff4115
Berend Wouters <berend.wouters@gmail.com>
2020-06-23 22:01:05 +0200

    Adds dockerignroe and fixes build

--- VNV.Gallery.App/gallery-app/src/app/services/blob.service.ts
+++ VNV.Gallery.App/gallery-app/src/app/services/blob.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BlobServiceClient } from '@azure/storage-blob';
-import { Image } from './Image';
+import { Image } from './image';
 
 @Injectable({
   providedIn: 'root'
