d7a10e3c93bfa8d44ac7a8d35fb207dfba8c221e
Berend Wouters <berend.wouters@gmail.com>
2020-06-30 19:41:24 +0200

    Fix mat-progress-bar import

--- gallery-app/src/app/app.module.ts
+++ gallery-app/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatCardModule } from  '@angular/material/card';
 import { MatButtonModule} from '@angular/material/button';
+import { MatProgressBarModule } from  '@angular/material/progress-bar'
 
 import { GalleryContentComponent } from './components/gallery-content/gallery-content.component';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
