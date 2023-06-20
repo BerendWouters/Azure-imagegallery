b0f2ea70a1a627637d6fde37bcccf02f4881af64
Berend Wouters <berend.wouters@gmail.com>
2023-06-20 19:04:15 +0200

    Angular material 15

--- gallery-app/src/app/app.module.ts
+++ gallery-app/src/app/app.module.ts
@@ -7,9 +7,9 @@ import { AppComponent } from './app.component';
 import { ListGalleriesComponent } from './components/list-galleries/list-galleries.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatCardModule } from  '@angular/material/card';
-import { MatButtonModule} from '@angular/material/button';
-import { MatProgressBarModule } from  '@angular/material/progress-bar'
+import { MatLegacyCardModule as MatCardModule } from  '@angular/material/legacy-card';
+import { MatLegacyButtonModule as MatButtonModule} from '@angular/material/legacy-button';
+import { MatLegacyProgressBarModule as MatProgressBarModule } from  '@angular/material/legacy-progress-bar'
 
 import { GalleryContentComponent } from './components/gallery-content/gallery-content.component';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
